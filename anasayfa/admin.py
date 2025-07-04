f1cfb68c59f668d3c4f54b8376964fa7313f241d
Eren Celik <erencelik2426@gmail.com>
2025-07-04 13:39:18 +0300

    .

--- anasayfa/admin.py
+++ anasayfa/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import InfoBox
+from django.contrib.auth.models import User
+
+from .models import InfoBox , RecruitmentStep
 
 @admin.register(InfoBox)  #decarator
 class InfoBoxAdmin(admin.ModelAdmin):
     list_display = ('title',)
+
+
+admin.site.register(RecruitmentStep)
