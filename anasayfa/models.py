938bd4834d421b30d33a611b283214d5068df264
Eren Celik <erencelik2426@gmail.com>
2025-07-04 13:39:35 +0300

    .

--- anasayfa/models.py
+++ anasayfa/models.py
@@ -8,3 +8,16 @@ class InfoBox(models.Model):
 
     def __str__(self):
         return self.title
+
+class RecruitmentStep(models.Model):
+    step_number = models.PositiveIntegerField()
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    color = models.CharField(max_length=7 , default="#1f4e79")
+    icon = models.CharField(max_length=50 , blank=True)
+
+    def __str__(self):
+        return f"{self.step_number:02d} - {self.title}"
+
+    class Meta:
+        ordering = ['step_number']
